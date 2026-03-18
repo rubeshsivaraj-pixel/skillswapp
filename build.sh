43edfaeede197f18197d7f4aa52f85a235c31d4f
rubeshsivaraj-pixel <rubeshsivaraj@gmail.com>
2026-03-18 14:17:32 +0530

    first deploy

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
-set -o errexit
+#!/usr/bin/env bash
 
-# Install dependencies
-pip install --upgrade pip
 pip install -r requirements.txt
-
-# Collect static files
 python manage.py collectstatic --noinput
-
-# Run database migrations
 python manage.py migrate
